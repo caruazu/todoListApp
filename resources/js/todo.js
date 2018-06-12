5e2ce6f8da64eb5956b8cd6ffdb06d6ec6a8303d
cauebasantos <cauebasantos@gmail.com>
2018-06-12 00:47:04 -0300

    Add some comments to the functions

--- resources/js/todo.js
+++ resources/js/todo.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 /*
 Function that return the @name get param on the @url
 */
@@ -12,20 +14,17 @@ function gup(name, url) {
     var regexS = "[\\?&]" + name + "=([^&#]*)";
     var regex = new RegExp(regexS);
     var results = regex.exec(url);
-    return results === null
-        ? null
+    return results === null ? null
         : results[1];
 }
 
-//Create our list_name that takes the "list" get param or use 'todoList' as default
-var list_name = (gup('list', location))
-    ? gup('list', location)
+//Create our list_name that takes the 'list' get param or use 'todoList' as default
+var list_name = (gup('list', location)) ? gup('list', location)
     : 'todoList';
 
 
 //Create our data to storage our to-do lists and completed lists on the local storage
-var data = (localStorage.getItem(list_name))
-    ? JSON.parse(localStorage.getItem(list_name))
+var data = (localStorage.getItem(list_name)) ? JSON.parse(localStorage.getItem(list_name))
     : {
         todo: [],
         complete: []
@@ -97,7 +96,6 @@ function confirmRemove() {
 Function that complete the to-do item or uncomplete the completed item
 */
 function completeItem() {
-    "use strict";
 
     var item = this.parentNode.parentNode;
     var parent = item.parentNode;
@@ -115,8 +113,7 @@ function completeItem() {
     localStorageUpdated();
 
     //Check if the item should be added to the completed list or re-added to the to-do list
-    var target = (id === 'todo')
-        ? document.getElementById('completed')
+    var target = (id === 'todo') ? document.getElementById('completed')
         : document.getElementById('todo');
 
     parent.removeChild(item);
@@ -127,8 +124,7 @@ function completeItem() {
 function addItemToDOM(text, completed) {
     "use strict";
 
-    var list = (completed)
-        ? document.getElementById('completed')
+    var list = (completed) ? document.getElementById('completed')
         : document.getElementById('todo');
 
     var item = document.createElement('li');
@@ -201,7 +197,9 @@ document.getElementById("item").addEventListener("keyup", function (event) {
     }
 });
 
-
+/*
+Function that hides the footer menu bar
+*/
 function hideMenuBar() {
     "use strict";
 
@@ -210,6 +208,9 @@ function hideMenuBar() {
     footer.setAttribute('style', 'display: none;');
 }
 
+/*
+Function that shows the footer menu bar
+*/
 function showMenuBar() {
     "use strict";
 
@@ -245,6 +246,9 @@ document.getElementById("add").addEventListener("click", function () {
     showMenuBar();
 });
 
+/*
+Control when to show or hide the footer menu bar
+*/
 var item = document.getElementById('item');
 
 item.addEventListener('mouseover', hideMenuBar);
